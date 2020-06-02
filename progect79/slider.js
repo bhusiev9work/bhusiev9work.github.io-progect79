5aa65bc8cf53f3c4c48ff08e1a3fd5a5a26bada9
bhusiev9work <66245909+bhusiev9work@users.noreply.github.com>
2020-06-02 04:04:31 +0300

    Add files via upload

---
+++ progect79/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	//Указываем что будем выводить по 3 слайда на экран
+
+$('.slider').slick({
+
+  infinite: true,
+
+  slidesToShow: 3,
+
+  slidesToScroll: 3
+
+});
+
+});
